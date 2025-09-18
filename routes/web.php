f37b19df7742dfc5cde8f2ec7c7f9c7089dffdd5
flaviasousa3112-star <flavia.sousa3112@gmail.com>
2025-09-18 10:47:47 -0300

    instalacao tailwind e teste de dados

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Ste', function() {
+    echo 'Olá Ste';
+
+});
+
+Route::get('/filmes/{filme}', function($filme) {
+    echo 'Acessando dados do filme: '. $filme;
+
+});
+
+Route::view('/conheca', 'sobre');
+
+
+
