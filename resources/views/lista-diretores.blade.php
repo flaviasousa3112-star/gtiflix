63eca7b89c759aa83595d275b3a5a8ecd17e4b09
flaviasousa3112-star <flavia.sousa3112@gmail.com>
2025-11-27 10:43:56 -0300

    Versão Final

--- resources/views/lista-diretores.blade.php
+++ resources/views/lista-diretores.blade.php
@@ -4,24 +4,25 @@
   <!-- Title -->
   <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
     <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Our team</h2>
-    <p class="mt-1 text-gray-600 dark:text-neutral-400">Diretores</p>
+    <p class="mt-1 text-black-600 dark:text-black-400">Diretores</p>
   </div>
   <!-- End Title -->
 
   <!-- Grid -->
-  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 md:gap-12">
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 md:gap-12 items-center justify-center">
     
   @foreach($diretores as $diretor)
   <div class="text-center">
-      <img class="rounded-full size-24 mx-auto" src="https://www.papodecinema.com.br/wp-content/uploads/2016/04/20180413-media-copy.webp" alt="Avatar">
-      <div class="mt-2 sm:mt-4">
-        <h3 class="font-medium text-gray-800 dark:text-neutral-200">
+      <img class="rounded-full size-30 mx-auto" src=" {{$diretor->foto}}" alt="Avatar">
+      <div class="mt-3 sm:mt-2">
+        <h3 class="font-medium text-black-800 dark:text-black-200">
         {{$diretor->nome}}
         </h3>
-        <p class="text-sm text-gray-600 dark:text-neutral-400">
+        <p class="text-sm text-black-600 dark:text-black-400">
         {{$diretor->descricao}}
         </p>
       </div>
+      
     </div>
     <!-- End Col -->
 @endforeach
--- resources/views/lista-produtoras.blade.php
+++ resources/views/lista-produtoras.blade.php
@@ -7,31 +7,25 @@
                     <div class="py-1 md:p-2 md:grid md:grid-cols-2 lg:grid-cols-3 gap-4">
                       <div class="flex flex-col">
                         <!-- Link -->
-                        <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
-                          <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">Support Docs</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Explore advice and explanations for all of Preline's features.</p>
+                          <div class="grow mt-10">
+                            <p class="font-medium text-sm text-black-800 dark:text-black-200">Temple Hill Entertainment</p>
+                            <p class="text-sm text-black-500 dark:text-black-500">Uma empresa estadunidense baseada em produção cinematográfica, criada em fevereiro de 2006</p>
                           </div>
                         </a>
                         <!-- End Link -->
 
                         <!-- Link -->
-                        <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="7" height="7" x="14" y="3" rx="1"/><path d="M10 21V8a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H3"/></svg>
-                          <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">Integrations</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Discover the huge range of tools that Preline integrates with.</p>
+                          <div class="grow mt-50">
+                            <p class="font-medium text-sm text-black-800 dark:text-black-200">Universal Pictures</p>
+                            <p class="text-sm text-black-500 dark:text-black-500">Uma produtora e distribuidora de filmes norte-americana.</p>
                           </div>
                         </a>
                         <!-- End Link -->
 
                         <!-- Link -->
-                        <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m7 11 2-2-2-2"/><path d="M11 13h4"/><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/></svg>
-                          <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">API Reference</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Build custom integrations with our first-class API.</p>
+                          <div class="grow mt-70">
+                            <p class="font-medium text-sm text-black-800 dark:text-black-200">Bluegrass film production</p>
+                            <p class="text-sm text-black-500 dark:text-black-500">Uma produtora de cinema americana fundada por Scott Stuber em 2012</p>
                           </div>
                         </a>
                         <!-- End Link -->
@@ -40,31 +34,24 @@
 
                       <div class="flex flex-col">
                         <!-- Link -->
-                        <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
+          
                           <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">Help Center</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Learn how to install, set up, and use Preline.</p>
+                        
                           </div>
                         </a>
                         <!-- End Link -->
 
                         <!-- Link -->
                         <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="4"/><path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-4 8"/></svg>
                           <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">Developer Hub</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Learn how to integrate or build on top of Preline.</p>
+
                           </div>
                         </a>
                         <!-- End Link -->
 
                         <!-- Link -->
                         <a class="p-3 flex gap-x-4 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 rounded-lg dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <svg class="shrink-0 size-4 mt-1 text-gray-800 dark:text-neutral-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
                           <div class="grow">
-                            <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">Community Forum</p>
-                            <p class="text-sm text-gray-500 dark:text-neutral-500">Learn, share, and connect with other Preline users.</p>
                           </div>
                         </a>
                         <!-- End Link -->
@@ -72,20 +59,37 @@
                       <!-- End Col -->
 
                       <div class="mt-2 md:mt-0 flex flex-col">
-                        <span class="ms-2.5 mb-2 font-semibold text-xs uppercase text-gray-800 dark:text-neutral-200">Customer stories</span>
 
                         <!-- Link -->
-                        <a class="p-3 flex gap-x-5 items-center rounded-xl hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" href="#">
-                          <img class="size-32 rounded-lg" src="https://images.unsplash.com/photo-1648737967328-690548aec14f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=320&q=80" alt="Avatar">
+                        <a class="p-3 flex items-center rounded-xl hover:outline-hidden" href="#">
+                          <img class="size-32 rounded-lg" src="https://www.veniceartcrawl.com/wp-content/uploads/2023/03/TempleHillEntertainment_200x200.png" alt="Avatar">
                           <div class="grow">
                             <p class="text-sm text-gray-800 dark:text-neutral-400">
-                              Preline Projects has proved to be most efficient cloud based project tracking and bug tracking tool.
                             </p>
-                            <p class="mt-3 inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 group-hover:underline group-focus:underline font-medium dark:text-blue-400">
-                              Learn more
-                              <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
-                            </p>
+                      
                           </div>
+
+                          <div class="mt-2 md:mt-0 flex flex-col">
+
+<!-- Link -->
+<a class="p-3 flex items-center rounded-xl hover:outline-hidden" href="#">
+  <img class="size-32 rounded-lg" src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Universal_Pictures_logo.svg" alt="Avatar">
+  <div class="grow m-45">
+    <p class="text-sm text-gray-800 dark:text-neutral-400">
+    </p>
+
+  </div>
+
+  <div class="mt-2 md:mt-0 flex flex-col">
+
+<!-- Link -->
+<a class="p-3 flex items-center rounded-xl hover:outline-hidden" href="#">
+  <img class="size-32 rounded-lg" src="https://pics.filmaffinity.com/018891942890198-nm_200.jpg" alt="Avatar">
+  <div class="grow mt-50">
+    <p class="text-sm text-gray-800 dark:text-neutral-400">
+    </p>
+
+  </div>
                         </a>
                         <!-- End Link -->
                       </div>
